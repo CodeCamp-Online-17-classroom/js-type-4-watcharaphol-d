54a531f8d34db56b3c184da149a4f5d7e591178a
watcharaphol.d <dangpiem@hotmail.com>
2024-06-22 00:43:54 +0700

    Create alert sum result of 2 inputs

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ### Lab13 Dynamic Type: Fundamental4
 - ให้ User กรอกข้อมูลที่เป็นตัวเลข 2 ครั้ง โดยใช้คำสั่ง prompt 
 - ให้ alert ผลบวกของตัวเลขทั้งสอง
+
+### Homework Finished 22/06/2567
+- นายวัชรพล แดงเปี่ยม (มายด์)
+- Online-CC#18
--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 // write code here
+let number1 = +prompt("Enter number 1:");
+let number2 = +prompt("Enter number 2:");
+
+alert((number1 && number2) ? `The sum result of ${number1}+${number2} = ${number1 + number2}` : `Cannot calculate due to the entered number is not Number-type`);
